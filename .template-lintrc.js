e0a57c0e2648e0a19a55f8e29a81087bbb65de47
Tristan F <26509014+LeoDog896@users.noreply.github.com>
2023-09-28 18:10:22 -0400

    chore: update

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,7 +1,6 @@
 'use strict';
 
 module.exports = {
-  extends: 'octane',
   rules: {
     'no-implicit-this': true,
     'block-indentation': 'tab',
--- testem.js
+++ testem.js
@@ -1,24 +1,23 @@
+'use strict';
+
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: [
-    'Chromium'
-  ],
-  launch_in_dev: [
-    'Chromium'
-  ],
+  launch_in_ci: ['Chrome'],
+  launch_in_dev: ['Chrome'],
+  browser_start_timeout: 120,
   browser_args: {
-    Chromium: {
+    Chrome: {
       ci: [
+        // --no-sandbox is needed when running Chrome inside a container
+        process.env.CI ? '--no-sandbox' : null,
         '--headless',
-        '--no-sandbox',
-        '--mute-audio',
-        '--disable-gpu',
-        '--window-size=1440,900',
         '--disable-dev-shm-usage',
-        '--remote-debugging-port=9222',
-        '--remote-debugging-address=0.0.0.0'
-      ]
-    }
-  }
+        '--disable-software-rasterizer',
+        '--mute-audio',
+        '--remote-debugging-port=0',
+        '--window-size=1440,900',
+      ].filter(Boolean),
+    },
+  },
 };
